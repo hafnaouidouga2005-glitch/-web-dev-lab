2918ee4f883199afbdf95b7af1a9193bfa674e4d
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-06 23:36:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-$(document).ready(function(){
+document.getElementById("addRow").addEventListener("click", function() {
 
-$("#addCourse").click(function(){
+let table = document.getElementById("courseTable");
+let row = table.insertRow();
 
-var row = 
-<tr>
-<td><input type="text" class="form-control"></td>
-<td><input type="number" class="form-control"></td>
-<td><input type="number" class="form-control"></td>
-<td><button class="btn btn-danger remove">Remove</button></td>
-</tr>
+row.innerHTML = 
+<td><input type="text" name="course[]" class="form-control"></td>
+<td><input type="number" name="credits[]" class="form-control"></td>
+<td><input type="number" name="grade[]" step="0.1" class="form-control"></td>
+<td><button type="button" class="btn btn-danger remove-row">Remove</button></td>
 ;
 
-$("table tbody").append(row);
-
 });
 
-$(document).on("click",".remove",function(){
-$(this).closest("tr").remove();
-});
 
-});
