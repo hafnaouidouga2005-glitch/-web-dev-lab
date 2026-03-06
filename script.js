34be4eade81e71a490b6cbd0351758e68e686d60
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-06 23:09:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 
-  $("#addCourse").click(function(){
-    var row = 
-      <tr>
-        <td><input type="text" class="form-control"></td>
-        <td><input type="number" class="form-control"></td>
-        <td><input type="number" class="form-control"></td>
-        <td><button class="btn btn-danger remove">Remove</button></td>
-      </tr>
-    ;
-    
-    $("table tbody").append(row);
-  });
+$("#addCourse").click(function(){
 
-  $(document).on("click",".remove",function(){
-    $(this).closest("tr").remove();
-  });
+var row = 
+<tr>
+<td><input type="text" class="form-control"></td>
+<td><input type="number" class="form-control"></td>
+<td><input type="number" class="form-control"></td>
+<td><button class="btn btn-danger remove">Remove</button></td>
+</tr>
+;
 
+$("table tbody").append(row);
 
+});
+
+$(document).on("click",".remove",function(){
+$(this).closest("tr").remove();
+});
+
+});
