969cd6bca4a4730012abd13b1bf7f738129631b1
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-06 22:18:17 +0100

    Create calculate.php

---
+++ calculate.php
@@ -0,0 +1,18 @@
+<?php
+$credits = $_POST['credits'];
+$grades = $_POST['grade'];
+
+$totalPoints = 0;
+$totalCredits = 0;
+
+for($i=0;$i<count($credits);$i++){
+
+$totalPoints += $credits[$i] * $grades[$i];
+$totalCredits += $credits[$i];
+
+}
+
+$gpa = $totalPoints / $totalCredits;
+
+echo "Your GPA is: ".number_format($gpa,2);
+?>
