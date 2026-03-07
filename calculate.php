814897790134f20e6c28cdc80f981cb73a654385
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-07 07:40:20 +0100

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -1,18 +1,57 @@
-<?php
-$credits = $_POST['credits'];
-$grades = $_POST['grade'];
-
-$totalPoints = 0;
+<?php 
+if (isset($_POST ['course'1, $_POST [' credits'], $_POST['grade'])){ 
+$courses         =$_POST ['course']; 
+$credits         =$_POST['credits'l;
+$grades          =$_POST ['grade];
+$totalPoints  = 0;
 $totalCredits = 0;
 
-for($i=0;$i<count($credits);$i++){
+echo "<table>";
+echo "<tr> 
+<th>Course</th>
+<th>Credits</th>
+<th>Grade</th>
+<th>Grade Points</th>
+</tr>"; 
+for  ($i=o; $i < count($courses); $i++) {
+  $course= htmlspecialchars ($courses [$i]);
+  $cr    =floatval($credits[sil);
+  $g     =floatval($grades [$i]);
+if ($cr <= o) continue;
+$pts           = $cr *$g; 
+stotalPoints  += $pts;
+$totalCredits += $cr;
+echo "<tr> 
+       <td>$course</td>
+       <td>$cr</td>
+       <td>$g</td> 
+       <td>$pts</td> 
+       </tr>";
+      }
+echo "</table>"; 
 
-$totalPoints += $credits[$i] * $grades[$i];
-$totalCredits += $credits[$i];
-
-}
-
-$gpa = $totalPoints / $totalCredits;
-
-echo "Your GPA is: ".number_format($gpa,2);
+if ($totalCredits >o){ 
+$gpa= $totalPoints / $totalcredits;
+ if ($gpa >= 3.7){ 
+   $interpretation-"Distinction"; 
+ }elseif ($gpa >= 3.0) { 
+ $interpretation = "Merit";
+ } elseif ($gpa >=2.0) { 
+ $interpretation = "Pass"; 
+ } else {  
+ $interpretation ="Fail"; 
+ }
+echo "<p>Your GPA is <strong>"  .number_format ($gpa, 2)  
+    .   "</strong> ($interpretation).</p>";  
+    } else { 
+echo "<p>No valid courses entered.</p>";
+  }
+  } else { 
+  echo "Data not received."; 
+  }  
 ?>
+  
+  
+  
+ 
+ 
