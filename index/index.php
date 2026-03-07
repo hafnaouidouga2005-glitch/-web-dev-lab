5d5e4c7ead61f8a236bb2a4238ae8463c8cc5064
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-07 08:46:53 +0100

    Update index.php

--- index/index.php
+++ index/index.php
@@ -1,3 +1,52 @@
+<?php 
+$result     = " ";
+$tableHtml  = " ";
+
+if ($_SERVER ['REQUEST_METHOD'] =='POST»){ 
+$courses         = $_POST ['course'] ?? [ ]; 
+$credits         =$_POST ['credits'] ?? [ ];
+$grades          =$_POST ['grade']   ?? [ ];
+$totalPoints  = 0;
+$totalCredits = 0; 
+
+$tableHtml = "<table>";
+$tableHtml .=くtr> 
+<th>Course</th><th>Credits</th>
+<th>Grade</th><th>Grade Points</th> 
+</tr>"; 
+for ($i = o; $i < count($courses); $i++){
+  $course   = htmlspecialchars ($courses [$i]);
+  $cr       = floatval($credits [$i]); 
+  $g        =floatval($grades [$i]); 
+  if ($cr <= o) continue; 
+  $pts = $cr * $g; 
+  $totalPoints += $pts; 
+  $totalCredits += $cr; 
+  $tableHtml.= "<tr>  
+  <td>$course</td><td>$cr</td>
+  <td>$g</td><td>$pts</td> 
+</tr>"; 
+}
+$tableHtml .="</table>"; 
+
+if ($totalCredits >0){
+  $gpa= $totalPoints / $totalCredits;
+  if ($gpa >= 3.7){
+    $interpretation="Distinction"; 
+} elseif ($gpa >= 3.0){
+$interpretation="Merit"; 
+}elseif ($gpa >= 2.0){
+ $interpretation = "Pass"; 
+} else {
+ $interpretation= "Fail"; 
+$result ="Your GPA is ".number_format ($gpa, 2) 
+         ."($interpretation)."; 
+ }else {
+$result="NO valid courses entered."; 
+ }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
