340800ac06eb3ad87ea349783bc3a150037666fb
hafnaouidouga2005-glitch <hafnaouidouga2005@gmail.com>
2026-03-08 06:46:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ row.className = "course-row";
 
 row.innerHTML =
 '<label>Course:</label>' +
-'<input type="text" name="course[]" placeholder="e.g. Mathematics" required>' +
-
+'<input type="text" name="course[]" ' +
+    'placeholder="e.g. Mathematics" required>' +
 '<label>Credits:</label>' +
-'<input type="number" name="credits[]" placeholder="e.g. 3" min="1" required>' +
-
+'<input type="number" name="credits[]" ' +
+  placeholder="e.g. 3" min="1" required>' +
 '<label>Grade:</label>' +
-
 '<select name="grade[]">' +
 '<option value="4.0">A</option>' +
 '<option value="3.0">B</option>' +
@@ -21,8 +20,8 @@ row.innerHTML =
 '<option value="0.0">F</option>' +
 '</select>' +
 
-'<button type="button" onclick="this.parentNode.remove()">Remove</button>';
-
+'<button type="button" ' +
+  'onclick="this.parentNode.remove()">Remove</button>';
 document.getElementById("courses").appendChild(row);
 }
 
@@ -35,7 +34,7 @@ var credits = document.querySelectorAll('[name="credits[]"]');
 
 for (var i = 0; i < courses.length; i++) {
 
-if (courses[i].value == "") {
+if (courses[i].value === "") {
 
 alert("All course name fields are required.");
 return false;
